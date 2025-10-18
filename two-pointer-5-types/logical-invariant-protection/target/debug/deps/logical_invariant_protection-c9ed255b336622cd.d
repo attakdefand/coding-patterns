e0dbf3a7dc8f15c5b8c908070cc54cbc9b711f89
root <root@DESKTOP-HGLJSE8.localdomain>
2025-10-18 13:37:13 +0700

    Auto-commit

---
+++ two-pointer-5-types/logical-invariant-protection/target/debug/deps/logical_invariant_protection-c9ed255b336622cd.d
@@ -0,0 +1,5 @@
+D:\28-patterns-attack-defense\two-pointer-5-types\logical-invariant-protection\target\debug\deps\logical_invariant_protection-c9ed255b336622cd.d: src\main.rs
+
+D:\28-patterns-attack-defense\two-pointer-5-types\logical-invariant-protection\target\debug\deps\logical_invariant_protection-c9ed255b336622cd.exe: src\main.rs
+
+src\main.rs:
